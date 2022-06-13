689e52bbd8b0acbbac5f3f6de41a3144f154a568
Constipated <ziertemha@gmail.com>
2022-06-12 20:21:10 -0500

    Made a graph

--- poll-frontend/src/App.tsx
+++ poll-frontend/src/App.tsx
@@ -1,10 +1,12 @@
 import React, { useState } from "react";
+import Graph from "./Graph/Graph";
 import "./App.css";
 
 function App() {
   return (
     <div className="App">
       <h1>Hey, do you wanna make a poll?</h1>
+      <Graph />
     </div>
   );
 }
