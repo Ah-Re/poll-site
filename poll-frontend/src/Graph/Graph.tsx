e5dfc51bd1eb339d6769aea7cf40077017c4c2ad
Constipated <ziertemha@gmail.com>
2022-06-25 18:54:03 -0500

    Made progress/poll bar

--- poll-frontend/src/Graph/Graph.tsx
+++ poll-frontend/src/Graph/Graph.tsx
@@ -1,8 +1,12 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import "./Graph.css";
 
 const Graph = () => {
-  return <div className="Graph"></div>;
+  return (
+    <div className="Graph">
+      <div className="graph-progress"></div>
+    </div>
+  );
 };
 
 export default Graph;
